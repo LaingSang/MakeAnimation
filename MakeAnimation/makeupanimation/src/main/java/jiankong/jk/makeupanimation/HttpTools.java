f93931884cfa184fba30245c959e4f13d23d1908
军卫 扬 <1808268458@qq.com>
2020-04-17 18:04:51 +0800

    修改httpTools一些内容

--- MakeAnimation/makeupanimation/src/main/java/jiankong/jk/makeupanimation/HttpTools.java
+++ MakeAnimation/makeupanimation/src/main/java/jiankong/jk/makeupanimation/HttpTools.java
@@ -1,11 +1,13 @@
 package jiankong.jk.makeupanimation;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 
 public class HttpTools {
-    public static OkHttpClient getClick(final boolean isHSlog, final String Message){
+    public static OkHttpClient getClick(final boolean isHSlog, final String Message,int redTimeOut,int connTimeOut){
         HttpLoggingInterceptor loggingInterceptor=new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
             @Override
             public void log(String message) {
@@ -19,11 +21,12 @@ public class HttpTools {
             }
         });
         loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient client=new OkHttpClient.Builder().addInterceptor(loggingInterceptor).build();
+        OkHttpClient client=new OkHttpClient.Builder().readTimeout(redTimeOut, TimeUnit.SECONDS).connectTimeout(connTimeOut,TimeUnit.SECONDS).addInterceptor(loggingInterceptor).build();
+
         return client;
     }
-    public  static Retrofit getRetrofit(final boolean isHSlog, String Message,String Url){
-        Retrofit retrofit=new Retrofit.Builder().baseUrl(Url).client(getClick(isHSlog,Message)).build();
+    public  static Retrofit getRetrofit(final boolean isHSlog, String Message,String Url,int redTimeOut,int connTimeOut){
+        Retrofit retrofit=new Retrofit.Builder().baseUrl(Url).client(getClick(isHSlog,Message,redTimeOut,connTimeOut)).build();
         return retrofit;
     }
 }
